d2584eb732eb79fd20a2484bb819cd4609b321d2
dom <davide.domini2@unibo.it>
2022-11-23 19:11:42 +0100

    feat(indexjs): add cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
+//const mongoose = require('mongoose')
+//const path = require('path');
+//const routes = require('./routes/routes');
+const cors = require('cors');
+
+//global.appRoot = path.resolve(__dirname);
 
 var PORT = 3000;
+//app.use('/static', express.static(__dirname + '/public')); 
+
+//mongoose.connect('mongodb://localhost:27017/DegreeWork');
+//routes(app);
 
 app.use(express.json());
+app.use(cors())
 
 app.use((req, res)=> {
     res.status(404).send({url: req.originalUrl + ' not found'})
