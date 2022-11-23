6615cb542ebecf2ba27e17c6e1ed6f83edd3b0c2
dom <davide.domini2@unibo.it>
2022-11-23 20:23:09 +0100

    fix(indexjs): fix frontend path

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app = express();
-//const mongoose = require('mongoose')
-//const path = require('path');
-//const routes = require('./routes/routes');
+const mongoose = require('mongoose')
+const path = require('path');
+const routes = require('./routes/routes');
 const cors = require('cors');
+const constants = require('../constants');
 
-//global.appRoot = path.resolve(__dirname);
+global.appRoot = path.resolve(__dirname);
 
 var PORT = 3000;
-//app.use('/static', express.static(__dirname + '/public')); 
 
-//mongoose.connect('mongodb://localhost:27017/DegreeWork');
-//routes(app);
+console.log(__dirname)
+
+app.use('/static', express.static(constants.FRONTEND_PATH)); 
+
+mongoose.connect('mongodb://localhost:27017/DegreeWork');
+routes(app);
 
 app.use(express.json());
 app.use(cors())
