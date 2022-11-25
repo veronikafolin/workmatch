1e3324a7ecb9c4f5d32d15325276cb6cf8a47ff2
dom <davide.domini2@unibo.it>
2022-11-25 19:18:33 +0100

    feat: add signup route

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,5 +1,6 @@
 module.exports = (app) => {
     var controller = require('../controllers/controller')
+    var authController = require('../controllers/authController')
     
     app
         .route('/api/greet/')
@@ -8,4 +9,8 @@ module.exports = (app) => {
     app
         .route('/api/students')
         .get(controller.list_students)
+
+    app
+        .route('/api/signup')
+        .post(authController.signup);
 }
