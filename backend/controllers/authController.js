aa6baa7d6ea2c5cfd6d02dc156dc6fceb76bad27
dom <davide.domini2@unibo.it>
2022-11-25 20:24:04 +0100

    feat: implement a first version of signup

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose');
 const Student = require("../models/student.js")(mongoose);
+const bcrypt = require('bcrypt');
+const jsonwebtoken = require('jsonwebtoken');
 
 exports.signup = (req, res) => {
-
+    var newUser = req.body.params;
+    Student
+        .findOne()
+        .where('username').equals(newUser.username)
+        .exec(function(err, student) {
+            if (student == null) {
+                //I can save it
+                const salt = bcrypt.genSaltSync(10);
+                const hash = bcrypt.hashSync(newUser.password, salt);
+                console.log(`signup, salt: ${salt} hash: ${hash}`)
+            } else{
+                res.json({message: 'Error! User already registered'})
+            }
+        })
 }
