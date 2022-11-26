8f5c664b02817280e5a5e6851a85bde1409c34aa
dom <davide.domini2@unibo.it>
2022-11-26 09:54:48 +0100

    feat(auth): implement signup skeleton

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,8 +14,19 @@ exports.signup = (req, res) => {
                 console.log(req.body)
                 const hash = bcrypt.hashSync(user.password, salt);
                 console.log(`signup, salt: ${salt} hash: ${hash}`)
+
+                //TODO --- salvare per bene 
+                var newStudent = new Student({
+                    email: 'alemme@gmail.com', username: user.username, password: hash, 
+                    grade: '100', name: 'Alessandro', surname: 'Magnani', school: { name: 'ITIS N.Baldini', type: 'Technical institute'}
+                });
+
+                newStudent.save(function(err, st){
+                    if(err) res.json({ message: 'Error! Retry later' });
+                    else res.json({ message: 'OK! User registerd! '});
+                })
             } else{
-                res.json({message: 'Error! User already registered'})
+                res.json({ message: 'Error! User already registered' })
             }
         })
 }
