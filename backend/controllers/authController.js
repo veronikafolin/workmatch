d7272500c6d5f7b2580b29a18eab919a2b26c5f7
dom <davide.domini2@unibo.it>
2022-11-26 16:01:04 +0100

    feat(auth): finish signup controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -11,16 +11,10 @@ exports.signup = (req, res) => {
         .exec(function(err, student) {
             if (student == null) {
                 const salt = bcrypt.genSaltSync(10);
-                console.log(req.body)
                 const hash = bcrypt.hashSync(user.password, salt);
-                console.log(`signup, salt: ${salt} hash: ${hash}`)
-
-                //TODO --- salvare per bene 
-                var newStudent = new Student({
-                    email: 'alemme@gmail.com', username: user.username, password: hash, 
-                    grade: '100', name: 'Alessandro', surname: 'Magnani', school: { name: 'ITIS N.Baldini', type: 'Technical institute'}
-                });
-
+                user.salt = salt;
+                user.password = hash;
+                var newStudent = new Student(user);
                 newStudent.save(function(err, st){
                     if(err) res.json({ message: 'Error! Retry later' });
                     else res.json({ message: 'OK! User registerd! '});
