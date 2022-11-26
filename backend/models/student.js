2ba66c1df7952245e0422d11f5c6e178d70457e3
dom <davide.domini2@unibo.it>
2022-11-26 16:00:37 +0100

    feat(model): add salt field

--- backend/models/student.js
+++ backend/models/student.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose');
 
 var StudentSchema = new mongoose.Schema({
     email: { type: String }, 
-    grade: { type: String },
     name: { type: String },
+    surname: { type: String },
     username: { type: String }, 
     password: { type: String },
+    salt: { type: String },
+    grade: { type: String },
     school: {
         name: { type: String },
         type: { type: String }
-    },
-    surname: { type: String }
+    }
 });
 module.exports = mongoose.model('Student', StudentSchema);
