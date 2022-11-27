e8f682e9f6b47d9e9956cf99491164bf76ac2e32
dom <davide.domini2@unibo.it>
2022-11-27 15:38:17 +0100

    feat(frontend): implement sigin component

---
+++ frontend/src/components/SigninForm.vue
@@ -0,0 +1,39 @@
+<script setup></script>
+
+<script>
+export default {
+    data(){
+        return {
+            form: {
+                username: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        signin(){
+            axios
+                .post('http://localhost:3000/api/login', {
+                    user: {
+                        username: this.form.username,
+                        password: this.form.password
+                    }
+                }).then(res => console.log(res))
+        }
+    }
+}
+</script>
+
+<template>
+    <form @submit.prevent="signin">
+        <div>
+            Username:
+            <input type="text" v-model="form.username" required/>
+        </div>
+        <div>
+            Password:
+            <input type="password" v-model="form.password" required/>
+        </div>
+        <button type="submit"> Submit </button>
+    </form>
+</template>
