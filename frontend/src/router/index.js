9c7c4051f46ee16fa0517dde1946b18a662b808a
dom <davide.domini2@unibo.it>
2022-11-28 19:42:17 +0100

    feat: add signin view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SignupView from '../views/SignupView.vue'
+import SigninView from '../views/SigninView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'signin',
+      component: SigninView
+    },
+    {
+      path: '/signup',
       name: 'signup',
       component: SignupView
     },
---
+++ frontend/src/views/SigninView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import SigninForm from '../components/SigninForm.vue'
+</script>
+
+<template>
+  <main>
+    <SigninForm />
+  </main>
+</template>
