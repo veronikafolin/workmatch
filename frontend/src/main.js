694f3a3732ae87f29b47bbce07f3a044faacc33f
dom <davide.domini2@unibo.it>
2022-11-28 18:18:12 +0100

    feat(frontend): add primevue import

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import PrimeVue from 'primevue/config';
+import './assets/main.css';
 
-import './assets/main.css'
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(router);
+app.use(PrimeVue);
 
-app.use(router)
-
-app.mount('#app')
+app.mount('#app');
