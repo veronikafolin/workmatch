6742fc90389fc891b9bc97fa18c74d3a825fedab
Veronika Folin <veronika.folin@studio.unibo.it>
2022-11-30 11:43:00 +0100

    feat: modifies init docker files to manage mongo db

--- database/init-mongodb/01-dbinit.js
+++ database/init-mongodb/01-dbinit.js
@@ -1,3 +1,6 @@
 var connection = new Mongo();
 var db = connection.getDB('workmatch'); //creates the db
-db.createCollection('students', function(err, collection){}) //creates the collection
+//creates the collection
+db.createCollection('students', function(err, collection){}) 
+db.createCollection('schools', function(err, collection){}) 
+db.createCollection('companies', function(err, collection){}) 
--- database/init-mongodb/02-init.sh
+++ database/init-mongodb/02-init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
 mongoimport --jsonArray --db workmatch --collection students --file tmp/data/Students.json
+mongoimport --jsonArray --db workmatch --collection schools --file tmp/data/Schools.json
+mongoimport --jsonArray --db workmatch --collection companies --file tmp/data/Companies.json
