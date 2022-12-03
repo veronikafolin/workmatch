318d2f44e42c33ed6978ffa94debdf3efdb467e9
dom <davide.domini2@unibo.it>
2022-12-03 11:44:38 +0100

    fix: fix companies import in init.sh

--- database/init-mongodb/02-init.sh
+++ database/init-mongodb/02-init.sh
@@ -2,4 +2,4 @@
 
 mongoimport --jsonArray --db workmatch --collection students --file tmp/data/Students.json
 mongoimport --jsonArray --db workmatch --collection schools --file tmp/data/Schools.json
-mongoimport --jsonArray --db workmatch --collection schools --file tmp/data/Companies.json
+mongoimport --jsonArray --db workmatch --collection companies --file tmp/data/Companies.json
