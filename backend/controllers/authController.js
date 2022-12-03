6e4cadf5d6c562ed3228abea5b5ee188a4154f36
dom <davide.domini2@unibo.it>
2022-12-03 11:53:49 +0100

    feat(backend): modify signup handler in order to support registration of multiple types of user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,26 +4,11 @@ const jsonwebtoken = require('jsonwebtoken');
 const SECRET_KEY = 'E79FB19FDC927E709F250F01CAFED631971E3ECD';
 
 exports.signup = (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    let type = req.body.type
     let user = req.body.user;
-    Student
-        .findOne()
-        .where('username').equals(user.username)
-        .exec((err, student) => {
-            if (student == null) {
-                const salt = bcrypt.genSaltSync(10);
-                const hash = bcrypt.hashSync(user.password, salt);
-                user.salt = salt;
-                user.password = hash;
-                var newStudent = new Student(user);
-                newStudent.save((err, st) => {
-                    if(err) res.json({ message: 'Error! Retry later' });
-                    else res.json({ message: 'OK! User registerd! '});
-                });
-            } else{
-                res.json({ message: 'Error! User already registered' })
-            }
-        });
+    if(type == 'student'){
+        registerStudent(user, res)
+    }
 }
 
 exports.login = (req, res) => {
@@ -58,3 +43,25 @@ exports.authorization = (token, id) => {
     }
     return valid;
 } 
+
+function registerStudent(user, res) {
+    res.header('Access-Control-Allow-Origin', '*');
+    Student
+        .findOne()
+        .where('username').equals(user.username)
+        .exec((err, student) => {
+            if (student == null) {
+                const salt = bcrypt.genSaltSync(10);
+                const hash = bcrypt.hashSync(user.password, salt);
+                user.salt = salt;
+                user.password = hash;
+                var newStudent = new Student(user);
+                newStudent.save((err, st) => {
+                    if(err) res.json({ message: 'Error! Retry later' });
+                    else res.json({ message: 'OK! User registerd! '});
+                });
+            } else{
+                res.json({ message: 'Error! User already registered' })
+            }
+        });
+}
