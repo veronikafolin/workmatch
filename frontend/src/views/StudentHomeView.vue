76eccfafeae08db908b41cebe81665d735cc19e7
dom <davide.domini2@unibo.it>
2022-12-19 18:50:08 +0100

    feat: add student home

---
+++ frontend/src/views/StudentHomeView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import StudentMenu from '../components/StudentMenu.vue'
+</script>
+
+<script>
+
+export default {
+  data() {
+       
+    },
+  methods:{
+    
+  },
+  mounted(){
+    
+  }
+}
+
+</script>
+
+
+<template>
+  <main>
+    <nav>
+        <StudentMenu/>
+    </nav>
+
+  </main>
+</template>
+
+
+
+<style>
+.p-card {
+  margin-top: 30px;
+}
+</style>
