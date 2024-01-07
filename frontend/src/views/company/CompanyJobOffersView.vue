ff701c0a759dc1cc0664a2a227f34dea96cd8ee5
Veronika Folin <veronika.folin@studio.unibo.it>
2024-01-07 20:39:13 +0100

    feat: company's view of company job offers

--- frontend/src/views/company/CompanyJobOffersView.vue
+++ frontend/src/views/company/CompanyJobOffersView.vue
@@ -11,7 +11,7 @@ import axios from "axios";
 export default{
   data() {
     return {
-      jobOffers: null,
+      jobOffers: [],
       messages: [],
       visible: false
     };
@@ -25,6 +25,21 @@ export default{
             this.jobOffers = res.data;
           });
     },
+    confirmDelete(jobOfferId) {
+      this.$confirm.require({
+        message: 'Are you sure you want to proceed?',
+        header: 'Confirmation',
+        icon: 'pi pi-exclamation-triangle',
+        rejectClass: 'p-button-text p-button-text',
+        acceptClass: 'p-button-danger p-button-text',
+        accept: () => {
+          this.deleteJobOffer(jobOfferId);
+        },
+        reject: () => {
+
+        }
+      });
+    },
     deleteJobOffer(jobOfferId){
       axios
           .delete(`http://localhost:3000/api/deleteJobOffer?id=${jobOfferId}`)
@@ -33,11 +48,18 @@ export default{
                 if (response.message.includes('Error')) {
                   this.messages.push({severity: 'error', content: response.message})
                 } else {
-                  this.messages.push({severity: 'success', content: response.message})
+                  this.messages.push({severity: 'success', content: response.message});
+                  this.requestJobOffers();
                 }
               }
           )
     },
+    receiveJobOfferFromChild(data) {
+      // Handle the received data from the child component
+      this.messages.push({severity: 'success', content: data.message})
+      this.jobOffers.push(data.result);
+      this.visible = false;
+    }
   },
   mounted() {
     this.requestJobOffers();
@@ -47,39 +69,56 @@ export default{
 </script>
 
 <template>
+
   <main>
+
     <nav>
       <CompanyMenu/>
     </nav>
 
     <div class="card flex p-3 border-round justify-content-center">
       <Button label="Insert Offer" icon="pi pi-plus" rounded @click="visible=true"></Button>
-      <Dialog v-model:visible="visible" modal :style="{ width: '50vw' }" header="Insert a new job offer">
-        <InsertJobOfferForm/>
+      <Dialog v-model:visible="visible" id="insert-job-offer-form">
+        <InsertJobOfferForm @send-data="receiveJobOfferFromChild"/>
       </Dialog>
     </div>
 
-    <div class="card justify-content-center">
+    <Message v-for="msg of messages" :severity="msg.severity" :sticky="false" :life="3000">{{msg.content}}</Message>
 
-      <Card v-for="offer in jobOffers">
+    <div class="card cards-container">
+
+      <Card class="single-card" v-for="offer in jobOffers">
+
         <template #title> {{offer.position}} </template>
-        <template #subtitle>
-          {{offer.place_of_work}} <br>
-          {{offer.working_hours}}h/week
-        </template>
-        <template #content>
-          {{offer.description}}
-        </template>
-        <template #footer>
-          <Button label="Delete offer" icon="pi pi-trash" @click="deleteJobOffer(offer._id)"/>
-        </template>
-      </Card>
 
-      <Message v-for="msg of messages" :severity="msg.severity">{{msg.content}}</Message>
+        <template #subtitle>
+          when: {{offer.timestamp}}
+        </template>
+
+        <template #content>
+          <strong> Curriculums requested: </strong>
+          <ul>
+            <li v-for="curriculum in offer.curriculums_requested"> {{curriculum}} </li>
+          </ul>
+
+          <strong> Working hours: </strong> {{offer.working_hours}}h/week <br>
+
+          <p>
+            <strong> Description: </strong>
+            {{offer.description}}
+          </p>
+        </template>
+
+        <template #footer>
+          <div class="button-container justify-content-end">
+            <Button id="button-danger" label="Delete" icon="pi pi-trash" @click="confirmDelete(offer._id)"/>
+          </div>
+        </template>
+
+      </Card>
 
     </div>
 
-
-
   </main>
+
 </template>
