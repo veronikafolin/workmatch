f1426ffd77ce96882024d0767e400f635764c025
Veronika Folin <veronika.folin@studio.unibo.it>
2024-01-07 20:53:49 +0100

    refactor: redirect to home page

--- frontend/src/components/SigninForm.vue
+++ frontend/src/components/SigninForm.vue
@@ -40,7 +40,11 @@ export default {
                     localStorage.userId = response.id
                     localStorage.userType = this.selectedRole
                     localStorage.username = this.form.username
-                    router.replace({name: `${this.selectedRole.toLowerCase()}home`})
+                    if(this.selectedRole === "Student"){
+                      router.replace({name: `studenthome`})
+                    } else {
+                      router.replace({name: `companyhome`})
+                    }
                 }
             });
         }
@@ -67,18 +71,17 @@ export default {
                 <InputText id="password" type="password" v-model="form.password" :feedback="false" placeholder="Password" required/>
             </span>
 
-<!--            <span class="p-input-icon-left">-->
-<!--                <Dropdown id="roleselector" v-model="selectedRole" :options="roles" placeholder="Select your role" class="w-full" />-->
-<!--            </span>-->
-
             <span class="p-input-icon-left role-selector-container">
               <label class="label" for="roleselector">Please select your role: </label>
               <SelectButton id="roleselector" v-model="selectedRole" :options="roles" aria-labelledby="single"/>
             </span>
 
+
             <Button class="w-full" type="submit" label="Login" />
 
-            <RouterLink to="/signup">Don't have an account yet? Sign Up</RouterLink>
+            <a href="/signup" class="w-full">
+              <Button class="w-full" label="Sign Up" id="button-signup"/>
+            </a>
 
             <Message v-for="msg of messages" :severity="msg.severity">{{msg.content}}</Message>
 
@@ -87,50 +90,3 @@ export default {
     </div>
 
 </template>
-
-<style>
-
-.title_form{
-  font-weight: bold;
-  font-size: 200%;
-  text-align: center;
-  font-family: 'Roboto', Arial, sans-serif;
-}
-
-.label{
-  font-family: 'Roboto', Arial, sans-serif;
-}
-
-#login {
-    margin: auto;
-    padding-top: 30px;
-    padding-bottom: 30px;
-    width: 40%;
-}
-
-#roleselector{
-    margin-bottom: 10px;
-    margin-top: 10px;
-}
-
-.role-selector-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-  margin-top: 10px;
-}
-
-.p-input-icon-left {
-  width: 100%;
-}
-
-@media screen and (max-width: 1000px) {
-
-    #login {
-        width: 80%;
-    }
-
-}
-
-</style>
