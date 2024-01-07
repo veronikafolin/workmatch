81a6c1637409c1b4779e07a35a323a4f8071b1e3
Veronika Folin <veronika.folin@studio.unibo.it>
2024-01-07 20:45:58 +0100

    refactor: form to insert a job offer

--- frontend/src/components/company/InsertJobOfferForm.vue
+++ frontend/src/components/company/InsertJobOfferForm.vue
@@ -19,9 +19,16 @@ export default {
     },
     methods:{
         insertJobOffer(){
+
+            var today = new Date();
+            var date = today.getDate() + '-' + (today.getMonth()+1) + '-' + today.getFullYear();
+            var time = today.getHours() + ":" + today.getMinutes();
+            var dateTime = date + ' ' + time;
+
             axios
                 .post('http://localhost:3000/api/insertJobOffer', {
                     jobOffer: {
+                          timestamp: dateTime,
                           position: this.form.position,
                           description: this.form.description,
                           place_of_work: this.form.place_of_work,
@@ -34,7 +41,7 @@ export default {
                   if (response.message.includes('Error')) {
                     this.messages.push({severity: 'error', content: response.message})
                   } else {
-                    this.messages.push({severity: 'success', content: response.message})
+                    this.$emit('send-data', response);
                   }
                 }
             );
@@ -52,40 +59,39 @@ export default {
 </script>
 
 <template>
-    <form @submit.prevent="insertJobOffer">
-        <div class="p-form-group-inline">
-            <div class="p-float-label">
-                <InputText id="position" type="text" v-model="form.position" required/>
-                <label for="position">Position </label>
-            </div>
-            <div class="p-float-label">
-              <TextArea id="description" type="text" v-model="form.description" required/>
-              <label for="description">Description </label>
-            </div>
-            <div class="p-float-label">
-                <InputText id="place_of_work" type="text" v-model="form.place_of_work" required/>
-                <label for="place_of_work">City </label>
-            </div>
-            <div class="p-float-label">
-              <InputText id="working_hours" type="text" v-model="form.phone_number" required/>
-              <label for="working_hours">Working hours </label>
-            </div>
-            <div class="p-float-label">
-              <MultiSelect id="curriculums" v-model="form.curriculums_requested" :options="this.curriculums" placeholder="Select curriculums requested"
-                           class="w-full md:w-20rem" />
-              <label for="curriculums">Curriculums </label>
-            </div>
-          <Button type="submit"> Submit </Button>
-          <Message v-for="msg of messages" :severity="msg.severity">{{msg.content}}</Message>
-        </div>
+    <form @submit.prevent="insertJobOffer" class="flex flex-column gap-3 align-items-center">
+
+      <span class="title_form">Insert a new job offer</span>
+
+      <div class="p-float-label">
+          <InputText id="position" type="text" v-model="form.position" required/>
+          <label for="position">Position </label>
+      </div>
+
+      <div class="p-float-label">
+        <TextArea id="description" type="text" v-model="form.description" required/>
+        <label for="description">Description </label>
+      </div>
+
+      <div class="p-float-label">
+          <InputText id="place_of_work" type="text" v-model="form.place_of_work" required/>
+          <label for="place_of_work">City </label>
+      </div>
+
+      <div class="p-float-label">
+        <InputText id="working_hours" type="text" v-model="form.working_hours" required/>
+        <label for="working_hours">Working hours </label>
+      </div>
+
+      <div class="p-float-label">
+        <MultiSelect id="curriculums" v-model="form.curriculums_requested" :options="this.curriculums" class="w-full md:w-14rem" />
+        <label for="curriculums">Curriculums </label>
+      </div>
+
+      <Button type="submit" > Submit </Button>
+
+      <Message v-for="msg of messages" :severity="msg.severity" :sticky="false" :life="3000">{{msg.content}}</Message>
+
     </form>
+
 </template>
-
-<style scoped>
-
-.p-float-label {
-    margin-top: 15px;
-    text-align: left;
-  }
-
-</style>
